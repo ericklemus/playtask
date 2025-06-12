4def238928c163faf83216e2308300b4ebb3b79b
Erick Lemus <elemusf@gmail.com>
2025-06-12 00:21:36 -0400

    feat(models): Add model taskList

--- app/infrastructure/database/models/base.py
+++ app/infrastructure/database/models/base.py
@@ -1,6 +1,7 @@
 import datetime as dt
 
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from uuid import UUID, uuid4
 
 
@@ -10,7 +11,21 @@ class Base(DeclarativeBase):
     updated_at: Mapped[dt.datetime] = mapped_column(default=dt.datetime.now)
 
 
+class TaskList(Base):
+    __tablename__ = "tasklists"
+
+    name: Mapped[str] = mapped_column(nullable=False)
+    tasks: Mapped[list["Task"]] = relationship(
+        "Task", back_populates="tasklist", cascade="all, delete"
+    )
+
+
 class Task(Base):
     __tablename__ = "tasks"
 
     name: Mapped[str] = mapped_column(nullable=False)
+    completed: Mapped[bool] = mapped_column(default=False)
+    tasklist_id: Mapped[UUID] = mapped_column(
+        ForeignKey("tasklists.uuid"), nullable=False
+    )
+    tasklist: Mapped["TaskList"] = relationship("TaskList", back_populates="tasks")
